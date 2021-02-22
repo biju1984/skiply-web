e3ad305c2e0d23662d4c51827535a8d3b020a8a1
Biju Joseph <biju.tharakan@rakbank.ae>
2021-02-22 18:11:33 +0400

    Imported skiply sdk

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import  {SkiplySession as Session}  from "skiply/build/umd";
+import { useEffect, useState } from "react";
 function App() {
+  const fetchData = async () => {
+    const res = await Session.appInfo();
+    setData(res);
+  };
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Fetching app info: {data?.status?.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
